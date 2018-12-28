da10f25616e9470397338a7161e8f3670796168f
grabe <mark@graybill.com>
2018-12-28 09:57:37 -0600

    Update ThreadContainer.cs

--- ThreadPoolExample/ThreadPoolExample/ThreadContainer.cs
+++ ThreadPoolExample/ThreadPoolExample/ThreadContainer.cs
@@ -1,10 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ThreadPoolExample
 {
+    public class ThreadContainer
+    {
+        private object Lock { get; } = new object();
+        private ThreadState _state = ThreadState.Unstarted;
+        private Thread Thread { get; set; }
+        private IThreadContext Context { get; set; } = null;
+        private ManualResetEvent ShutdownEvent { get; } = new ManualResetEvent(false);
+        private AutoResetEvent ResumeEvent { get; } = new AutoResetEvent(false);
 
+        public ThreadState State { get { lock (Lock) { return _state; } } }
+        public ManualResetEvent TerminatedEvent { get; } = new ManualResetEvent(false);
+
+        public ThreadContainer()
+        {
+            Thread = new Thread(new ThreadStart(DoWork));
+            Thread.Start();
+        }
+
+        public bool ExecuteContext(IThreadContext context)
+        {
+            bool successfulInvoke = false;
+
+            lock (Lock)
+            {
+                successfulInvoke = context != null && _state == ThreadState.Suspended;
+                if (successfulInvoke)
+                {
+                    Context = context;
+                    _state = ThreadState.Running;
+                    ResumeEvent.Set();
+                }
+            }
+
+            return successfulInvoke;
+        }
+
+        public void Shutdown(bool wait = true)
+        {
+            lock (Lock)
+            {
+                _state = ThreadState.StopRequested;
+            }
+
+            ShutdownEvent.Set();
+            ResumeEvent.Set();
+            Thread.Join();
+        }
+
+        private void DoWork()
+        {
+            do
+            {
+                Suspend();
+
+                if (!ShutdownEvent.WaitOne(0))
+                    Context.DoWork();
+
+            } while (!ShutdownEvent.WaitOne(0));
+
+            lock (Lock)
+            {
+                _state = ThreadState.Stopped;
+            }
+
+        }
+
+        private void Suspend()
+        {
+            lock (Lock)
+            {
+                _state = ThreadState.Suspended;
+            }
+
+            ResumeEvent.WaitOne();
+        }
+    }
 }
